425c19986512e6b52dfaf8602fa936a45b7ed854
core.editor <tygrenvoltaris150@gmail.com>
2026-02-08 13:50:31 +0200

    modifications on the CNN model

---
+++ dataloader/dataloader.py
@@ -0,0 +1,34 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_dataloaders(data_dir, batch_size=32, img_size=224):
+
+    train_transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.RandomRotation(20),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()   # normalization [0,1]
+    ])
+
+    val_test_transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor()
+    ])
+
+    train_dataset = datasets.ImageFolder(
+        f"{data_dir}/Train", transform=train_transform
+    )
+
+    val_dataset = datasets.ImageFolder(
+        f"{data_dir}/Validation", transform=val_test_transform
+    )
+
+    test_dataset = datasets.ImageFolder(
+        f"{data_dir}/Test", transform=val_test_transform
+    )
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader   = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader  = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader, train_dataset.classes
